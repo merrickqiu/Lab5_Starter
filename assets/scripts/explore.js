d4a5894c015733e31769440089d985e3d5201a0d
Merrick <merrickqiu@gmail.com>
2023-05-03 17:22:51 -0700

    Explore and Expand

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,56 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
-
 function init() {
-  // TODO
+  // Define Elements
+  let selector = document.getElementById("voice-select");
+  let button = document.querySelector("button");
+  let text = document.getElementById("text-to-speak");
+  let face = document.querySelector("img");
+  let utterThis = new SpeechSynthesisUtterance(text.value);
+
+  // Set default to select
+  selector.value = "select";
+
+  // Get voices and populate selector
+  const synth = window.speechSynthesis;
+  let voices = [];
+  voices = synth.getVoices();
+  for (let i = 0; i < voices.length; i++){
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+    if (voices[i].default) {
+      option.textContent += " — DEFAULT";
+    }
+    option.setAttribute("data-lang", voices[i].lang);
+    option.setAttribute("data-name", voices[i].name);
+    
+    selector.appendChild(option);
+  }
+
+  // Play audio
+  button.addEventListener('click', function() {
+    //face.src = "assets/images/smiling-open.png";
+    utterThis.text = text.value;
+    const selectedOption =
+      selector.selectedOptions[0].getAttribute("data-name");
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+    synth.speak(utterThis);
+    //face.src = "assets/images/smiling.png";
+  
+  })
+
+  // Change image
+  utterThis.onstart = function() {
+    face.src = 'assets/images/smiling-open.png';
+  };
+  
+  // Set up the onend event handler
+  utterThis.onend = function() {
+    face.src = 'assets/images/smiling.png';
+  };
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,67 @@
 // expose.js
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+    // Define Elements
+    let selector = document.getElementById("horn-select");
+    let button = document.querySelector("button");
+    let audio = document.querySelector("audio");
+    let volume = document.getElementById("volume");
+
+    let images = document.querySelectorAll('img');
+    let image = images[0];
+    let volumeIcon = images[1];
+
+    const jsConfetti = new JSConfetti();
+    
+    // Set default to select
+    selector.value = "select"
+    
+    // Change voice and image
+    selector.addEventListener('change', function() {
+      switch (selector.value) {
+        case "select":
+          image.src = "assets/images/no-image.png";
+          audio.src = "";
+        case "air-horn":
+          image.src = "assets/images/air-horn.svg";
+          audio.src = "assets/audio/air-horn.mp3";
+          break;
+        case "car-horn":
+          image.src = "assets/images/car-horn.svg"
+          audio.src = "assets/audio/car-horn.mp3";
+          break;
+        case "party-horn":
+          image.src = "assets/images/party-horn.svg"
+          audio.src = "assets/audio/party-horn.mp3";
+          break;
+      }
+    }) 
+  
+    // Play audio
+    button.addEventListener('click', function() {
+      audio.play();
+      if (selector.value == "party-horn") {
+        jsConfetti.addConfetti();
+      }
+    })
+
+    // Volume control
+    volume.addEventListener('change', function() {
+      audio.volume = volume.value / 100;
+
+      if (volume.value == 0) {
+        volumeIcon.src = "assets/icons/volume-level-0.svg";
+        volumeIcon.alt = "Volume level 0";
+      } else if (volume.value < 33) {
+        volumeIcon.src = "assets/icons/volume-level-1.svg";
+        volumeIcon.alt = "Volume level 1";
+      } else if (volume.value < 67) {
+        volumeIcon.src = "assets/icons/volume-level-2.svg";
+        volumeIcon.alt = "Volume level 2";
+      } else {
+        volumeIcon.src = "assets/icons/volume-level-3.svg";
+        volumeIcon.alt = "Volume level 3";
+      }
+    })
 }
